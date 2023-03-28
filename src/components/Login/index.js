110e9e32e62373e5b1d09a9ba7f26b117bfdb87a
rajashekarpodicheti <rajashekarpodicheti1@gmail.com>
2023-03-28 12:26:22 +0000

    main

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -55,7 +55,7 @@ class Login extends Component {
           id="username"
           value={username}
           className="input-element"
-          placeholder="Ex-Puneet"
+          placeholder="Ex-Rajashekar"
           onChange={this.onChangeUsername}
         />
       </div>
@@ -75,7 +75,7 @@ class Login extends Component {
           id="password"
           value={password}
           className="input-element"
-          placeholder="Ex-puneet@2021"
+          placeholder="Ex-rajashekar@2021"
           onChange={this.onChangePassword}
         />
       </div>
